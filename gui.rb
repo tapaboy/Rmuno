61ec317dd79a5c2c8ec53c14b974086f61e4d97e
tapaboy <tapaboy.shadowmoon@gmail.com>
2021-12-19 20:20:24 +0900

    README.mdを追加

--- gui.rb
+++ gui.rb
@@ -3,24 +3,8 @@ require 'jumanpp_ruby'
 require 'yaml'
 require "./ask"
 
-=begin
-#辞書読み込み
-dict_noun = []
-dict_noun_man = []
-dict_noun_place = []
-dict_noun_sahen = []
-dict_noun_time = []
-dict_sextet = []
-
-dict = [dict_noun, dict_noun_man, dict_noun_place, dict_noun_sahen, dict_noun_time, dict_sextet]
-
-dict.each do |i|
-  begin
-    i = YAML.load_file"./dict/#{dict[i]}"
-  rescue
-  end
-end
-=end
+#立ち絵画像は、「三日月アルペジオ」（http://roughsketch.en-grey.com/）さんから
+#頂いたものを元にしています。
 
 #ここからウィンドウ描画
 #ウィンドウ透過のための設定
@@ -58,43 +42,44 @@ end
 class View < TranparentWindow
   def initialize (yome='ちょっと、今日は何してたか話しなさいよ。別にあなたのことを知りたいわけじゃないのよ。ただのヒマつぶしよ。')
     view = TranparentWindow.new
-    view.set_default_size(300, 300)
+    view.set_default_size(280, 770)
 
     #プログラム側文章表示欄
     text_area = Gtk::TextView.new
-    text_area.set_size_request(100, 30)
+    text_area.set_size_request(300, -1)
     text_area.buffer.text = yome
-
     scrolled_area = Gtk::ScrolledWindow.new
-    scrolled_area.set_size_request(200, -1)
+    scrolled_area.set_size_request(300, -1)
     scrolled_area.set_policy(:automatic, :never)  # スクロールバー；横は自動、縦は表示しない。
     scrolled_area.add(text_area) 
 
     #テキスト入力欄
     entry_area = Gtk::Entry.new
-    entry_area.set_size_request(200, -1)
+    entry_area.set_size_request(300, -1)
+    entry_area.buffer.text = 'ここに書きなさいよね'
     entry_area.signal_connect("activate") do
       put_text(entry_area, text_area)
     end
-
     entry_area.overwrite_mode=(true)
-    box = Gtk::Box.new(:horizontal)
-    label = Gtk::Label.new('何か言ってみ')
-    box.pack_start(label, :expand => false, :fill => false, :padding => 0)
-    box.pack_start(entry_area, :expand => false, :fill => false, :padding => 0)
+
+#    box = Gtk::Box.new(:horizontal)
+#    label = Gtk::Label.new('何か言ってみ')
+#    box.pack_start(label, :expand => false, :fill => false, :padding => 0)
+#    box.pack_start(entry_area, :expand => false, :fill => false, :padding => 0)
 
     button = Gtk::Button.new(:label => '自爆')
     button.set_size_request(100, 20)
     button.signal_connect("clicked") {Gtk.main_quit}
 
-    bgpxbuf = GdkPixbuf::Pixbuf.new(file: 'bg.png')
+    bgpxbuf = GdkPixbuf::Pixbuf.new(file: 'chara.png')
     bgimage = Gtk::Image.new(pixbuf: bgpxbuf)
 
     fixed = Gtk::Fixed.new
-    fixed.put(bgimage, 0, 0)
-    fixed.put(scrolled_area, 100,200)
-    fixed.put(box, 50,235)
-    fixed.put(button, 100, 270)
+#    fixed.put(bgimage, 0, 340)
+    fixed.put(bgimage, 0, 340)
+    fixed.put(scrolled_area, 0,580)
+    fixed.put(entry_area, 0, 640)
+    fixed.put(button, 100, 720)
     view.add(fixed)
 
     view.show_all
@@ -104,7 +89,6 @@ class View < TranparentWindow
 
   def put_text (entry, textview)
     ore_talk = entry.text
-    @ore_talk = entry.text
     StoreWords.new.get_words (ore_talk)
     textview.buffer.text = "ふーん、無駄に生きてるのね。聞いて損したわ。それから？"  # 
     entry.text = ''     # Entryの文字を消す
